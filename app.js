b6edaa11a3e634f8fd96a9821e5d8404284cbef8
Abhishek Yadav <rulersab@gmail.com>
2023-06-18 00:13:20 +0530

    Add user login api. Code refactor.
    Add user login api.
    Code refactor.

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
 const express = require("express");
 const jsonwebtoken = require("jsonwebtoken");
 const con = require('./users')
-// The secret should be an unguessable long string (you can use a password generator for this!)
-const JWT_SECRET =
-  "10";
+
+const JWT_SECRET = "dsff10312413#@$!@#$@#$!#@$erwdfa";
 
 const app = express();
 app.use(express.json());
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  console.log(`${username} is trying to login ..`);
-  console.log(`${password} is trying to login ..`);
+require('./routes/user')(app);
 
-  if (username === "admin" && password === "admin") {
-    return res.json({
-      token: jsonwebtoken.sign({ user: "admin" }, JWT_SECRET),
-    });
-  }
-
-  return res
-    .status(401)
-    .json({ message: "The username and password your provided are invalid" });
-});
-app.get("/super", (req, res) => {
+app.use(function(req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(401).json({ error: "Not Authorized" });
+    return res.status(401).json({ error: "User Not Authorized" });
   }
-
-  // Bearer <token>>
-  const authHeader = req.headers.authorization;
-  const token = authHeader.split(" ")[1];
-
+  // This is a Bearer token
+  var token = req.headers.authorization.split(" ")[1];
   try {
-    // Verify the token is valid
     const { user } = jsonwebtoken.verify(token,JWT_SECRET);
-    console.log(`user ${user}`);
-    return res.status(200).json({
-      message: `Congrats ${user}! You can now accesss the super secret resource`,
-    });
+    console.log("the user object " + user);
+    req.user = user
+    next()
   } catch (error) {
-    console.log("error is "+error);
-    return res.status(401).json({ error: "Not Authorized in catch" });
+    console.log("error while getting user details "+error);
+    return res.status(401).json({ error: "Not Authorized user" });
   }
 });
-con.connect ((error) =>{
-  if(error)console.log("error found", error);
-  else console.log("connection succesful");
-});
+
+require('./routes/journal')(app);
+
 app.listen(3001, () => {
   console.log("API running on localhost:3001");
 });
